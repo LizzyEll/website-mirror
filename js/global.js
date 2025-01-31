b46c90510838c736c4d01186ab720af408ff72b8
Elizabeth _ <lizzy@lunarware.tech>
2025-01-31 01:38:00 -0600

    ruh roh

--- js/global.js
+++
@@ -1,6 +0,0 @@
-let main = document.getElementById("main");
-
-function clamp(number, min, max) {
-    return Math.min(Math.max(number, min), max);
-}
-
--- js/index.js
+++ js/index.js
@@ -1,9 +1,16 @@
+let main = document.getElementById("main");
+
+function clamp(number, min, max) {
+    return Math.min(Math.max(number, min), max);
+}
+
+
 function loadScript(url) {
     var script = document.createElement("script");
     script.src = url;
     document.head.appendChild(script);
 }
 
-loadScript("js/global.js");
 loadScript("js/dvdlogo.js");
 loadScript("js/rainbow.js");
+
