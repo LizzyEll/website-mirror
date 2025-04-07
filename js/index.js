c841829eeaae7f11791d9b818d65b890c01c861c
LizzyEll <Lizzy@lunarware.tech>
2025-04-07 07:48:57 -0500

    start fresh

--- js/index.js
+++
@@ -1,9 +0,0 @@
-function loadScript(url) {
-    var script = document.createElement("script");
-    script.src = url;
-    document.head.appendChild(script);
-}
-
-loadScript("js/dvdlogo.js");
-loadScript("js/rainbow.js");
-
