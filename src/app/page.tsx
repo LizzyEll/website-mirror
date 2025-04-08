b79e2ab605b2cc667dec1e28b55dc647ddf40692
LizzyEll <Lizzy@lunarware.tech>
2025-04-08 06:57:16 -0500

    Fix main page metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,31 @@
 import BoxLink from "@/app/components/boxLink";
+import { type Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Portfolio",
+    authors: [{ name: "LizzyEll" }],
+    keywords: ["Home", "Portfolio", "Git", "Gitlab", "GitHub", "LizzyEll", "LizzyEll00", "Lunarware", "Lunarware.xyz", "Lunarware.tech"],
+    description: "My home page",
+    openGraph: {
+        title: "Portfolio",
+        description: "My home page",
+        url: "https://lunarware.tech/",
+        siteName: "Portfolio",
+        images: [
+            {
+                url: "https://lunarware.xyz/images/avatar.webp",
+                width: 800,
+                height: 600,
+                alt: "Avatar of LizzyEll",
+                type: "image/webp",
+            },
+        ],
+        locale: "en_US",
+        type: "website",
+    },
+    icons: [{ rel: "icon", url: "/images/avatar.webp" }],
+};
+
 export default function HomePage() {
   return (
       <div className="flex flex-wrap justify-center">
